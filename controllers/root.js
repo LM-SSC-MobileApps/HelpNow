96a975c807ac429b13ec20557313155e75e1d7b8
Doug Falconieri <douglas.d.falconieri@lmco.com>
2015-09-17 09:45:37 -0600

    Added prototype of internationalization

--- controllers/root.js
+++ controllers/root.js
@@ -1,4 +1,4 @@
-angular.module("helpNow").controller("RootCtrl", function($scope) {
+angular.module("helpNow").controller("RootCtrl", function($scope, $http) {
 	var currentView = "events";
 	var currentLanguage = "Eng";
 	
@@ -12,9 +12,28 @@ angular.module("helpNow").controller("RootCtrl", function($scope) {
 	
 	$scope.setCurrentLanguage = function(language) {
 		currentLanguage = language;
+		if (language == "Ben")
+			$http.get("i18n/text-BEN.json")
+				.success(function (data) {
+					$scope.text = data;
+				})
+				.error(function (data) {
+					alert(data);
+				});
+		else {
+			$http.get("i18n/text-ENG.json")
+				.success(function (data) {
+					$scope.text = data;
+				})
+				.error(function (data) {
+					alert(data);
+				});
+		}	
 	}
 	
 	$scope.getLanguageClass = function(language) {
 		return currentLanguage == language ? "active" : "";
 	}
+	
+	$scope.setCurrentLanguage("Eng");
 });
