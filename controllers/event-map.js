68a9098c6b04dd557993a9a75a0b7e36520e7dd9
jonuht <jonathan.d1.uht@lmco.com>
2015-09-29 14:49:36 -0600

    Cleaned up Map Code in event-map.js

--- controllers/event-map.js
+++ controllers/event-map.js
@@ -10,22 +10,14 @@ angular.module("helpNow").controller("EventMapCtrl", function ($scope, $routePar
 	$scope.lat = $routeParams.lat;
 	$scope.long = $routeParams.long;
 	$scope.eventRadius = $routeParams.eventRadius;
-	$scope.eventDate = $routeParams.eventDate;
+	$scope.eventDate = $routeParams.eventDate.replace(/-/g, "/");
 	$scope.eventTime = $routeParams.eventTime;
 	$scope.contactPhone = $routeParams.contactPhone;
 });
 
 angular.module("helpNow").directive('map', function () {
     return {
-        scope: {
-            location: '&location'
-        },
-        link: function (scope) {
-            var testData = {
-                max: 8,
-                data: [{ lat: 24.6408, lng: 46.7728, count: 3 }, { lat: 23.7301, lng: 90.3065, count: 2 }]
-            };
-
+        link: function () {
             var baseLayer = L.tileLayer(
               'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
@@ -33,33 +25,9 @@ angular.module("helpNow").directive('map', function () {
               }
             );
 
-            var cfg = {
-                // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-                // if scaleRadius is false it will be the constant radius used in pixels
-                "radius": 200,
-                "maxOpacity": .8,
-                // scales the radius based on map zoom
-                "scaleRadius": false,
-                // if set to false the heatmap uses the global maximum for colorization
-                // if activated: uses the data maximum within the current map boundaries
-                //   (there will always be a red spot with useLocalExtremas true)
-                "useLocalExtrema": true,
-                // which field name in your data represents the latitude - default "lat"
-                latField: 'lat',
-                // which field name in your data represents the longitude - default "lng"
-                lngField: 'lng',
-                // which field name in your data represents the data value - default "value"
-                valueField: 'count'
-            };
-
-
-            var heatmapLayer = new HeatmapOverlay(cfg);
-
             var map = new L.map('map', {
-                layers: [baseLayer/*, heatmapLayer*/]
+                layers: [baseLayer]
             }).setView([eventLat, eventLong], 13);
-
-            heatmapLayer.setData(testData);
 
             L.control.scale().addTo(map);
 
@@ -69,15 +37,6 @@ angular.module("helpNow").directive('map', function () {
                 }
                 locationOutline = L.circle(e.latlng, overlayRadius).addTo(map);
             });
-
-            /*var buildingOutline = L.polygon([
-              [39.557040,-105.165888],
-              [39.554856,-105.163232],
-              [39.555838,-105.161837],
-              [39.557963,-105.164502]
-            ]).addTo(map);
-        
-            buildingOutline.bindPopup("Non-Existant Deer Creek Facility");*/
         }
     };
 });
