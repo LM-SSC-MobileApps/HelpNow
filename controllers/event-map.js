41100a7fedb7113cb0a37b7ac6baaec95684cd5c
jonuht <jonathan.d1.uht@lmco.com>
2015-10-06 16:04:52 -0600

    Additions to the guest help request areas, event map, and event resources

--- controllers/event-map.js
+++ controllers/event-map.js
@@ -1,6 +1,8 @@
 angular.module("helpNow").controller("EventMapCtrl", function ($scope, $routeParams) {
     $scope.eventID = $routeParams.eventID * 1;
-	$scope.event = $scope.getEvent($scope.eventID);
+    $scope.event = $scope.getEvent($scope.eventID);
+    $scope.resources = $scope.getResourcesForEvent($scope.eventID);
+    $scope.resource = $scope.resources[0];
 	$scope.setCurrentView("event-map");
 	
 	$scope.initMap = function(map) {
@@ -10,5 +12,47 @@ angular.module("helpNow").controller("EventMapCtrl", function ($scope, $routePar
 			}
 			locationOutline = L.circle(e.latlng, overlayRadius).addTo(map);
 		});
+
+		var firstAidIcon = L.icon({
+		    iconUrl: 'style/images/icons/FirstAidResource.png',
+		    iconSize: [128, 158.25]
+		});
+
+		var waterIcon = L.icon({
+		    iconUrl: 'style/images/icons/WaterResource.png',
+		    iconSize: [72, 73]
+		});
+
+		var evacuationIcon = L.icon({
+		    iconUrl: 'style/images/icons/EvacuationIcon.png',
+		    iconSize: [72, 73]
+		});
+
+		for (var i = 0; i < $scope.resources.length; i++) {
+		    var resource = $scope.resources[i];
+		    var resourceIcon;
+		    if (resource.resourceType == "First Aid") {
+		        resourceIcon = firstAidIcon;
+		    }
+		    else if (resource.resourceType == "Water") {
+		        resourceIcon = waterIcon;
+		    }
+		    else if (resource.resourceType == "Evacuation Site") {
+		        resourceIcon = evacuationIcon;
+		    }
+		    var resourceMarker = L.marker([resource.lat, resource.long], { icon: resourceIcon }).addTo(map);
+		    resourceMarker.on('click', function (e) {
+		        for (var j = 0; j < $scope.resources.length; j++) {
+		            var resourceLatLng = L.latLng($scope.resources[j].lat, $scope.resources[j].long);
+		            if(e.latlng.lat == resourceLatLng.lat && e.latlng.lng == resourceLatLng.lng)
+		            {
+		                $scope.resource = $scope.resources[j];
+		                $scope.$apply();
+		            }
+		        }
+		        if ($scope.resource.resourceType == "Evacuation Site") showResourcePanel(false);
+		        else showResourcePanel(true);
+		    });
+		}
 	}
 });
--- controllers/root.js
+++ controllers/root.js
@@ -36,6 +36,16 @@ angular.module("helpNow").controller("RootCtrl", function($scope, $http) {
 	}
 	
 	$scope.setCurrentLanguage("Eng");
+
+	$scope.getResourcesForEvent = function (eventID) {
+	    var resources = [];
+	    for (var i = 0; i < $scope.resources.length; i++) {
+	        var resource = $scope.resources[i];
+	        if (resource.eventID == eventID) resources.push(resource);
+	    }
+	    if (resources.length > 0) return resources;
+	    return {};
+	}
 	
 	$scope.getEvent = function(eventID) {
 		for (var i = 0; i < $scope.events.length; i++) {
@@ -44,18 +54,76 @@ angular.module("helpNow").controller("RootCtrl", function($scope, $http) {
 		}
 		return {};
 	}
+
+	$scope.resources = [
+        {
+            id: 1,
+            eventID: 1,
+            resourceType: "First Aid",
+            lat: 23.718,
+            long: 90.355,
+            provider: "NGO #1",
+            message: ""
+        },
+        {
+            id: 2,
+            eventID: 1,
+            resourceType: "First Aid",
+            lat: 23.719,
+            long: 90.374,
+            provider: "NGO #2",
+            message: ""
+        },
+        {
+            id: 3,
+            eventID: 1,
+            resourceType: "First Aid",
+            lat: 23.765,
+            long: 90.398,
+            provider: "NGO #3",
+            message: ""
+        },
+        {
+            id: 4,
+            eventID: 1,
+            resourceType: "Water",
+            lat: 23.767,
+            long: 90.41,
+            provider: "NGO #4",
+            message: ""
+        },
+        {
+            id: 5,
+            eventID: 1,
+            resourceType: "Evacuation Site",
+            lat: 23.73,
+            long: 90.365,
+            provider: "",
+            message: "No more than 1 bag per person.  Only bring what you can carry."
+        },
+        {
+            id: 6,
+            eventID: 1,
+            resourceType: "Evacuation Site",
+            lat: 23.75,
+            long: 90.43,
+            provider: "",
+            message: "No more than 1 bag per person.  Only bring what you can carry."
+        }
+	]
 	
 	$scope.events = [
 		{
 			id: 1,
 			location: "Dhaka, Bangladesh",
 			eventType: "Flood",
-			lat: 23.713,
+			lat: 23.74,
 			long: 90.39,
 			eventRadius: "542 km",
 			eventDate: "9/23/2015",
 			eventTime: "4:36 PM",
-			contactPhone: "+880 2 555 5555"
+			contactPhone: "+880 2 555 5555",
+		    iconPath: "style/images/icons/FloodIcon.png"
 		},
 		{
 			id: 2,
@@ -66,7 +134,9 @@ angular.module("helpNow").controller("RootCtrl", function($scope, $http) {
 			eventRadius: "1024 km",
 			eventDate: "9/24/2015",
 			eventTime: "2:16 PM",
-			contactPhone: "+880 2 555 5555"
+			contactPhone: "+880 2 555 5555",
+			iconPath: "style/images/icons/TsunamiIcon.png"
+
 		},
 		{
 			id: 3,
@@ -77,7 +147,8 @@ angular.module("helpNow").controller("RootCtrl", function($scope, $http) {
 			eventRadius: "860 km",
 			eventDate: "9/25/2015",
 			eventTime: "1:06 PM",
-			contactPhone: "+880 2 555 5555"
+			contactPhone: "+880 2 555 5555",
+			iconPath: "style/images/icons/EarthquakeIcon.png"
 		}
 	]
 });
