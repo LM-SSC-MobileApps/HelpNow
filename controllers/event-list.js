3e4b5880395aaaa3f3c381b5a5b40de5f363e043
Doug Falconieri <douglas.d.falconieri@lmco.com>
2015-10-13 12:00:53 -0600

    Added code to dynamically place map markers from database

--- controllers/event-list.js
+++ controllers/event-list.js
@@ -1,11 +1,25 @@
 angular.module("helpNow").controller("EventListCtrl", function($scope) {
 	var map;
 	
+	function addEventsToMap() {
+		if (!map || !$scope.events) return;
+		angular.forEach($scope.events, function(event) {
+			var eventIcon = L.icon({
+				iconUrl: $scope.getIcon(event.EventType.Description),
+				iconSize: [43, 44]
+			}); 
+			L.marker([event.EventLocations[0].LAT, event.EventLocations[0].LONG], { icon: eventIcon }).addTo(map)
+		})
+	}
+	
 	$scope.setCurrentView("events");
 	
-	$scope.$on("EventDataLoaded", function() {});
+	$scope.$on("EventDataLoaded", function() {
+		addEventsToMap()
+	});
 	
 	$scope.initMap = function(newMap) {
 		map = newMap;
+		addEventsToMap();
 	}
 });
