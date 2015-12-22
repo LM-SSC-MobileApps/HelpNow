911d8a053441abbfd15575305b5da54260906c94
dsjennin <dave.s.jennings@lmco.com>
2015-12-22 15:38:55 -0700

    add switch to demo logic

--- controllers/demo.js
+++ controllers/demo.js
@@ -9,11 +9,13 @@ angular.module("helpNow").controller("DemoCtrl", ["$scope", "DemoService", "Even
 
 
         $scope.startDemo = function () {
-            // console.log($scope.started);
             $scope.started = true;
-            var event = demoEvents[0];
-            var eventResponse = Event.save(event);
+            var event = demoData[0];
 
+            Event.save(event, function (data) {
+                $scope.newEvent = data.json;
+                console.log($scope.newEvent);
+            });
 
         };
 
@@ -22,14 +24,62 @@ angular.module("helpNow").controller("DemoCtrl", ["$scope", "DemoService", "Even
             $location.path(path);
         };
 
-        var demoEvents = [
+
+        var demoData = [
+
 
             {
-                "EventTypeID": 1,
-                "OrganizationID": 1,
-                "Summary": "Flood",
-                "Active": "true"
+                "ID": 1,
+                "Type": "Event",
+                "WaitTime": 0,
+                "Data": {
+                    "EventTypeID": 1,
+                    "OrganizationID": 1,
+                    "Summary": "Flood",
+                    "Active": "true",
+                    "CreateDate": "2015-12-22 00:00:00"
+                }
+            },
+            {
+                "ID": 2,
+                "Type": "EventLocation",
+                "WaitTime": 2,
+                "Data": {
+
+                    "Description": "Dhaka, Bangladesh",
+                    "LAT": "23.713",
+                    "LONG": "90.39",
+                    "Radius": 542
+                }
             }
+
         ];
+
+
+        angular.forEach(demoData, function (item, key) {
+            console.log(item.ID);
+            console.log(item.Type);
+            switch (item.Type) {
+
+                case "Event":
+                {
+                    console.log("I'm an event");
+                    //TODO: Event.save goes here
+
+                    break;
+                }
+                case "EventLocation":
+                {
+                    console.log("I'm an event location");
+                    //TODO: EventLocation.save goes here...
+                    break;
+                }
+                default:
+                    console.log("I'm lost");
+
+            }
+
+        });
+
 
     }]);
