c25d56a6c3db4621bd412ea66b76b5f641621ac7
mmajor <michael.t.major@lmco.com>
2016-01-10 13:45:42 -0700

    In work commit

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -656,6 +656,30 @@
         return false;
     };
 
+    //var hnNextDirective = {
+    //    'hnNext': ['$parse', function ($parse) {
+    //        return {
+    //            restrict: 'A',
+    //            require: 'form',
+    //            link: function (scope, formElement, attributes, formController) {
+
+    //                var fn = $parse(attributes.rcSubmit);
+
+    //                formElement.bind('btn-next', function (event) {
+    //                    // if form is not valid cancel it.
+    //                    if (!formController.$valid) return false;
+
+    //                    scope.$apply(function () {
+    //                        fn(scope, { $event: event });
+    //                    });
+    //                });
+    //            }
+    //        };
+    //    }]
+    //};
+
+
+
     $scope.fieldIsRequired = function () {
 
     };
