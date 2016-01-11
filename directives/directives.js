5ae03f580625cb1fc817d7ba3c16707583b1ab06
Michael Major <michael.t.major@lmco.com>
2016-01-11 10:16:35 -0700

    final language additions for inventory

--- directives/directives.js
+++ directives/directives.js
@@ -1,26 +1,26 @@
 angular.module("helpNow").directive('showErrors', function () {
-      return {
-          restrict: 'A',
-          require: '^form',
-          link: function (scope, el, attrs, formCtrl) {
-              // find the text box element, which has the 'name' attribute
-              var inputEl = el[0].querySelector("[name]");
-              // convert the native text box element to an angular element
-              var inputNgEl = angular.element(inputEl);
-              // get the name on the text box so we know the property to check
-              // on the form controller
-              var inputName = inputNgEl.attr('name');
+    return {
+        restrict: 'A',
+        require: '^form',
+        link: function (scope, el, attrs, formCtrl) {
+            // find the text box element, which has the 'name' attribute
+            var inputEl = el[0].querySelector("[name]");
+            // convert the native text box element to an angular element
+            var inputNgEl = angular.element(inputEl);
+            // get the name on the text box so we know the property to check
+            // on the form controller
+            var inputName = inputNgEl.attr('name');
 
-              // only apply the has-error class after the user leaves the text box
-              inputNgEl.bind('blur', function () {
-                  el.toggleClass('has-error', formCtrl[inputName].$invalid);
-              })
+            // only apply the has-error class after the user leaves the text box
+            inputNgEl.bind('blur', function () {
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
 
-              scope.$on('show-errors-check-validity', function () {
-                  el.toggleClass('has-error', formCtrl[inputName].$invalid);
-              });
-          }
-      }
+            scope.$on('show-errors-check-validity', function () {
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
+        }
+    };
 });
 
 angular.module("helpNow").directive('latitude', function () {
@@ -51,7 +51,7 @@ angular.module("helpNow").directive('longitude', function () {
                     // not validating for empty value, only for valid
                     return true;
                 }
-                return LONG_REGEXP.test(viewValue)
+                return LONG_REGEXP.test(viewValue);
             };
         }
     };
