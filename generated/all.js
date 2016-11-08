5c75915092354f8c1856e95e899f6b24522f7e35
EC2 Default User <ec2-user@ip-172-31-24-2.ap-northeast-1.compute.internal>
2016-11-08 19:07:39 +0000

    Updated english message for no distribution center

--- generated/all.js
+++ generated/all.js
@@ -4544,6 +4544,14 @@ angular.module("helpNow").directive('map', ['MapLayer', function (MapLayer) {
     };
 }]);
 
+angular.module('helpNow').factory('ResourceRequest', function ($resource) {
+    return $resource('api/resourcerequest/:id', null ,{
+        update: {
+            method: 'PUT'
+        }
+    });
+});
+
 angular.module('helpNow').factory('Account', function ($resource) {
     return $resource('api/account/:id', null ,{
         update: {
@@ -4676,13 +4684,6 @@ angular.module('helpNow').factory('ResourceLocationTransport', function ($resour
 });
 angular.module('helpNow').factory('ResourceLocationType', function ($resource) {
     return $resource('api/resourcelocationtype/:id', null, {
-        update: {
-            method: 'PUT'
-        }
-    });
-});
-angular.module('helpNow').factory('ResourceRequest', function ($resource) {
-    return $resource('api/resourcerequest/:id', null ,{
         update: {
             method: 'PUT'
         }
