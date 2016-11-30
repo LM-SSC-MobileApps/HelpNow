9a557c941ce341c92cabf6fadd5eec23e91b3446
jonuht <jonathan.d1.uht@lmco.com>
2016-11-30 12:29:36 -0700

    Recommiting the model file with heatmap min/max values and added updates to new-event.html

--- generated/all.js
+++ generated/all.js
@@ -4554,14 +4554,6 @@ angular.module("helpNow").directive('map', ['MapLayer', function (MapLayer) {
     };
 }]);
 
-angular.module('helpNow').factory('ResourceRequest', function ($resource) {
-    return $resource('api/resourcerequest/:id', null ,{
-        update: {
-            method: 'PUT'
-        }
-    });
-});
-
 angular.module('helpNow').factory('Account', function ($resource) {
     return $resource('api/account/:id', null ,{
         update: {
@@ -4694,6 +4686,13 @@ angular.module('helpNow').factory('ResourceLocationTransport', function ($resour
 });
 angular.module('helpNow').factory('ResourceLocationType', function ($resource) {
     return $resource('api/resourcelocationtype/:id', null, {
+        update: {
+            method: 'PUT'
+        }
+    });
+});
+angular.module('helpNow').factory('ResourceRequest', function ($resource) {
+    return $resource('api/resourcerequest/:id', null ,{
         update: {
             method: 'PUT'
         }
