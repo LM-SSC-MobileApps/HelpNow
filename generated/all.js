b313829e01f017c001c0cb0f62b34a36d8895b18
jason.m.mathews <jason.m.mathews@lmco.com>
2016-12-01 21:53:10 -0700

    Updated some colors and made all icons opaque

--- generated/all.js
+++ generated/all.js
@@ -3855,6 +3855,7 @@ angular.module("helpNow").controller("RootCtrl", ["$scope", "$route", "$location
     var currentLanguage = "Eng";
     var currentView = "";
 
+
     $scope.eventsResource = $resource("/api/event");
     $scope.currentUser = JSON.parse(sessionStorage.getItem("user"));
     if ($scope.currentUser != null) {
@@ -4554,6 +4555,14 @@ angular.module("helpNow").directive('map', ['MapLayer', function (MapLayer) {
     };
 }]);
 
+angular.module('helpNow').factory('ResourceRequest', function ($resource) {
+    return $resource('api/resourcerequest/:id', null ,{
+        update: {
+            method: 'PUT'
+        }
+    });
+});
+
 angular.module('helpNow').factory('Account', function ($resource) {
     return $resource('api/account/:id', null ,{
         update: {
@@ -4686,13 +4695,6 @@ angular.module('helpNow').factory('ResourceLocationTransport', function ($resour
 });
 angular.module('helpNow').factory('ResourceLocationType', function ($resource) {
     return $resource('api/resourcelocationtype/:id', null, {
-        update: {
-            method: 'PUT'
-        }
-    });
-});
-angular.module('helpNow').factory('ResourceRequest', function ($resource) {
-    return $resource('api/resourcerequest/:id', null ,{
         update: {
             method: 'PUT'
         }
